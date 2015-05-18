2a56e219db6ac5d6d72a0039ad0067bf6e091af7
Matvik-PC <Matvik1103@gmail.com>
2015-05-19 00:49:06 +0300

    Начал писать интерфейс репозитория.

---
+++ Queue/Queue/Models/Interface/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Queue.Models;
+
+namespace Queue.Models.Interface
+{
+    public interface IRepository
+    {
+        #region Client Methods
+        bool EnterQueue(int UID, int QID); //Пользователь с U-ID хочет встать в очередь на запрос Q-ID
+        bool LeaveQueue(int UID, int QID); //Пользователь с U-ID хочет покинуть очередь на запрос Q-ID
+
+        #endregion
+
+        #region Expert Methods
+        bool HandleQuery(int EID, int QID); //Эксперт с E-ID хочет подписаться на запрос Q-ID
+        bool AdoptClient(int EID, int QID); // Эксперт принимает первого в очереди
+        bool SendToSubQueue(int UID, int SubQID); // Эксперт посылает клиента U-ID вне очереди на запрос SubQID
+        
+        #endregion
+
+        #region Admin Methods
+        bool ClearProcessing(int EID); //Отписать эксперта от всех запросов
+        bool AddQuery(QueryView query); //Добавить новый вид запроса
+
+        #endregion
+
+        #region List Methods
+        List<QueryView> SelectAllQuery(); // Возвращает список всех запросов 
+
+        #endregion
+    }
+}
---
+++ Queue/Queue/Models/QueryView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Queue.Models
+{
+    public class QueryView
+    {
+        public string NameQuery { get; set; }
+        public int ExpertsCount { get; set; }
+        public int QueueCount { get; set; }
+        public int SubQueueCount { get; set; }
+    }
+}
